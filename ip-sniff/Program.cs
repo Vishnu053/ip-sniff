fd4213c730936c50a9e379d3313eb0d36f304033
vishnu053 <vishnunair053@gmail.com>
2022-04-09 11:37:28 +0530

    Added guard and checking

--- ip-sniff/Program.cs
+++ ip-sniff/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Drawing;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace ip_sniff
 {
@@ -18,6 +21,7 @@ namespace ip_sniff
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to IP Sniff!");
+            Console.WriteLine($"Your IP address is {GetLocalIPAddress()}");
             AcceptIpFromUser();
             ScanIp();
         }
@@ -40,33 +44,97 @@ namespace ip_sniff
         private static void ScanIp()
         {
             Console.Clear();
-            foreach (int s in PortsToScan)
-            // for(int s =0; s<10000;s++)
+            //check if its localhost or ip address
+            if (CheckIfValidIP(IP))
             {
-                using (TcpClient Scan = new TcpClient())
+                //checked if the ip is live
+                if (PingAddress(IP))
                 {
-                    try
+                    Console.WriteLine($"{IP} is up");
+                    foreach (int s in PortsToScan)
+                    // for(int s =0; s<10000;s++)
                     {
-                        Scan.Connect(IP, s);
-                        Console.WriteLine($"[{s}] is open", Color.Green);
-                        if(showRunningServices==true)
+                        using (TcpClient Scan = new TcpClient())
                         {
-                            ScanServicesInPort(s);
-                        }
-                    }
-                    catch
-                    {
-                        if (showOnlyRunningPorts == false)
-                        {
-                            Console.WriteLine($"[{s}] is closed", Color.Red);
-                        }
+                            try
+                            {
+                                Scan.Connect(IP, s);
+                                Console.WriteLine($"[{s}] is open", Color.Green);
+                                if (showRunningServices == true)
+                                {
+                                    ScanServicesInPort(s);
+                                }
+                            }
+                            catch
+                            {
+                                if (showOnlyRunningPorts == false)
+                                {
+                                    Console.WriteLine($"[{s}] is closed", Color.Red);
+                                }
 
+                            }
+                        }
                     }
                 }
+                else
+                {
+                    Console.WriteLine($"{IP} is down. Cannot perform further actions until the IP comes back up.");
+                }
             }
+            else
+            {
+                Console.WriteLine($"What you provided ({IP}) does not seem to be a valid IP address.");
+
+            }
+        }
+        //function to get local IP address
+        private static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        
+        private static bool CheckIfValidIP(string ipAddr)
+        {
+            Match match = Regex.Match(ipAddr, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+            return match.Success;
         }
         private static void ScanServicesInPort(int port){
             Console.WriteLine($"Scanning services on port {port}");
+        }
+        private static bool PingAddress(string address)
+        {
+            bool pingable = false;
+            using(var pinger= new Ping())
+            {
+                try
+                {
+                    PingReply reply = pinger.Send(address);
+                    pingable = reply.Status == IPStatus.Success;
+
+                }
+                catch (PingException)
+                {
+                    pingable = false;
+                    throw;
+                }
+                finally
+                {
+                    if(pinger != null)
+                    {
+                        pinger.Dispose();
+                    }
+                }
+
+                return pingable;
+            }
         }
     }
 }
